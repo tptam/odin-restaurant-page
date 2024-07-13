7e3a0b340fecd6085bce9271f4cf668d971841a4
tptam <99264397+tptam@users.noreply.github.com>
2024-07-14 06:00:46 +0900

    Implement home module to add HTML elements to doc

--- src/home.js
+++ src/home.js
@@ -1,7 +1,28 @@
-import mainImage from './images/tables.jpg';
+import ImgTables from './images/tables.jpg';
 
 function home(){
-    console.log("Welcome to Tam's!");
+    const content = document.querySelector("#content");
+    const imgMain = new Image();
+    imgMain.src = ImgTables;
+    content.appendChild(imgMain);
+
+    const homeTextHtml = `
+        <div id="home-text">
+            <h1>Welcome to Tam's! </h1>
+            <p>
+                Dine beneath a canopy of leaves, feeling as though you've discovered a secret garden in the heart of the city.
+            </p>
+        </div>
+    `;
+    const homeText = parseHtml(homeTextHtml);
+    content.appendChild(imgMain);
+    content.appendChild(homeText);
+}
+
+function parseHtml(htmlText) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(htmlText, 'text/html');
+    return doc.body.firstElementChild;
 }
 
 export default home;
