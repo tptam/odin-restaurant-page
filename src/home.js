18357cde102afef9beaad4a0c415cb80d748b531
tptam <99264397+tptam@users.noreply.github.com>
2024-07-15 12:14:22 +0900

    Add footer updating function for home

--- src/home.js
+++ src/home.js
@@ -2,6 +2,21 @@ import parseHtml from "./parseHtml.js"
 import ImgTables from './images/tables.jpg';
 import './style.css';
 
+function updateFooterAssets(){
+    const assetTexts = [
+        'Photo by <a href="https://unsplash.com/@ferhadd?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Farhad Ibrahimzade</a> on <a href="https://unsplash.com/photos/a-room-filled-with-lots-of-green-plants-K8OPYyAEtvI?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>',
+        ];
+    
+    const assets = document.querySelector("footer .assets");
+    for (let text of assetTexts) {
+        const assetHtml = `
+            <span class="asset-item">${text}</span>
+        `;
+        const asset = parseHtml(assetHtml);
+        assets.appendChild(asset);
+    }
+}
+
 function home(){
     const content = document.querySelector("#content");
     content.setAttribute("data-tab", "home");
@@ -23,6 +38,8 @@ function home(){
     const homeText = parseHtml(homeTextHtml);
     content.appendChild(imgMain);
     content.appendChild(homeText);
+
+    updateFooterAssets();
 }
 
 export default home;
